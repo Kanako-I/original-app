56c8e0cb3fd01f249c24fa2b61448b698c717b5e
Kanako-I <kanako.8ki@gmail.com>
2020-11-17 21:30:14 +0900

    チャット画面の編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
   def index
+    # @message = Message.new
+    # @rooms = Room.find(params[:room_id])
+    # @messages = @room.messages.includes(:user)
   end
 end
